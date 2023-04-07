8df5e72c78c6c6421316ded071878aa1a37dfa77
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-04-07 18:58:35 +0100

    styling and fetching data
    fetch data using axios, and displayed them using usestate and useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,27 @@ import TMDB from "./api/TMDB";
 import { useEffect, useState } from "react";
 import MovieCard from "./Components/MovieCard";
 
-
 function App() {
   const [randomMovie, setRandomMovie] = useState([]);
 
   useEffect(() => {
     const fetchMovie = async () => {
-      const {data} = await TMDB.get('discover/movie')
-      setRandomMovie(data.results)
-    }
+      const { data } = await TMDB.get("discover/movie");
+      setRandomMovie(data.results.slice(0, 1));
+    };
 
-    fetchMovie()
+    fetchMovie();
   }, []);
 
   return (
-    <div className="text-blue-500">
-      {randomMovie.map((randomMovie, index) => {
-        return <MovieCard key={index} {...randomMovie} />
-      })}
-    </div>
-  )
+    <>
+      <div class="bg-cover bg-no-repeat bg-[url('./images/hero-bg.jpg')] bg-gray-700 bg-blend-multiply w-full h-screen pt-24">
+        {randomMovie.map((randomMovie) => {
+          return <MovieCard {...randomMovie} />;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 
 function getPosterURL(posterPath) {
-  return `https://image.tmdb.org/t/p/w500${posterPath}`
+  return `https://image.tmdb.org/t/p/w500${posterPath}`;
 }
 
-const MovieCard = ({ poster_path, title, release_date }) => {
+const MovieCard = ({ poster_path, title, release_date, overview }) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="text-amber-500">
       <img
         src={getPosterURL(poster_path)}
         alt={title}
-        className="w-[300px] h-[450px] shadow-sm rounded-md"
+        className="shadow-md rounded-md object-cover text-center mx-auto w-48 lg:w-72"
       />
-      <div className="flex flex-col px-3 w-[150px]">
+
+      <div className="text-center mt-4">
         <h1 className="font-bold">{title}</h1>
         <p className="font-normal text-slate-500">{release_date}</p>
+        <p className=" w-96 mx-auto">{overview}</p>
+
+        <input
+          type="text"
+          placeholder="Guess the Year"
+          id="year"
+          class="border text-sm rounded-lg block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 mx-auto mt-8 text-center"
+        />
       </div>
     </div>
   );
