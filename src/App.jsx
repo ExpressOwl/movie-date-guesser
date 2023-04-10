8b4507fd87e9c73ab715df83c064bf1a31b01c35
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-04-10 03:40:16 +0100

    .env added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ import MovieCard from "./Components/MovieCard";
 function App() {
   const [randomMovie, setRandomMovie] = useState([]);
 
+  axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    headers: {
+      Accept: "application/json"
+    },
+    params: {
+      api_key: `process.env.REACT_APP_API_KEY`,
+      region: 'US',
+      sort_by: 'popularity.desc',
+      include_adult: false,
+      page: Math.floor(Math.random() * 500),
+    }
+  })
+
   useEffect(() => {
     const fetchMovie = async () => {
       const { data } = await TMDB.get("discover/movie");
