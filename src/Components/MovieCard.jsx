a971fc1bc2e8940ed83725d31b7cc058ed99bf4f
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-04-10 04:15:40 +0100

    Hid the release date for the movies...

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -162,7 +162,7 @@ const MovieCard = ({ poster_path, title, release_date, overview }) => {
             >
               {title}
             </h1>
-            <p className="font-normal text-slate-500">{release_date}</p>
+            <p className="font-normal text-slate-500 hidden">{release_date}</p>
             <p className="mx-auto w-72 sm:w-96 md:w-[600px] text-[#e9e6e6]">
               {overview}
             </p>
